04efafc42d7c95bebee45f842b53e71885ac08bb
Thomas Lambert <lambertthomas10@gmail.com>
2021-11-24 10:15:07 +0100

    styles

--- theme/smartweb/js/smartweb.js
+++ theme/smartweb/js/smartweb.js
@@ -85,5 +85,55 @@ $(document).ready(function () {
     var div = $('#subsite-navigation > ul');
     var hasHorizontalScrollbar = div.scrollWidth > div.clientWidth;
     console.log(document.querySelectorAll('#subsite-navigation > ul').scrollWidth);
+    // stop bootstrap carousel
+    $('.carousel').each(function () {
+        $(this).carousel({
+            interval: false
+        });
+    });
+    // test new slider
+
+    $('.swiper').each(function (index) {
+        var batchSize = $(this).attr('data-nb-results-by-batch')
+        console.log($(this).attr('data-nb-results-by-batch'));
+        var mySwiper = new Swiper($(this)[0],{
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            spaceBetween:10,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            breakpoints: {
+                // when window width is >= 320px
+                320: {
+                  slidesPerView: 1,
+                  slidesPerGroup: 1,
+                },
+                // when window width is >= 480px
+                576: {
+                  slidesPerView: batchSize > 1 ?2:1,
+                  slidesPerGroup: batchSize > 1 ?2:1
+                },
+                // when window width is >= 640px
+                768: {
+                  slidesPerView: batchSize > 2 ?2:batchSize,
+                  slidesPerGroup: batchSize > 2 ?2:batchSize
+                },
+                992: {
+                    slidesPerView: batchSize > 2 ?3:batchSize,
+                    slidesPerGroup: batchSize > 2 ?3:batchSize
+                },
+                1200: {
+                slidesPerView: batchSize,
+                slidesPerGroup: batchSize
+                }
+              }
+        });
+    });
 });
 
