495a66b80dc1ef293610098f9e7e57152b293e58
Christophe Boulanger <christophe.boulanger@imio.be>
2022-02-14 16:47:57 +0100

    Refactor esirius fragment to have waiting level colors

--- theme/smartweb/fragments/esirius.py
+++ theme/smartweb/fragments/esirius.py
@@ -10,21 +10,33 @@ def message(self):
         f"https://passerelle-braine-l-alleud.test.entrouvert.org/esirius-swi/prod/get_all_indicators?apikey={esirius_apikey}")
     data = response.json()
     if data.get("data") is None:
-        return ""
-    if data.get("data")[0].get("closed") is True:
-        return "closed"
+        return
+
+    data = data.get("data")[0]
+    if data.get("closed") is True:
+        return {"closed": True}
+
+    level = None
     # str au format "HH:MM:SS"
-    time = data.get("data")[0].get("estimatedAvgWaitingTime")
-    if time == "-":
-        # Same as closed is True?
-        value = "<span class=\"value\">--</span>"
-        unit = "<span class=\"unit\">minute</span>"
-        return "{}{}".format(value, unit)
+    time = data.get("estimatedAvgWaitingTime")
     hours, minutes, seconds = time.split(":")
     seconds = int(seconds)
     hours = int(hours) * 60
     minutes = int(minutes) + 1 if seconds > 1 else int(minutes)
     time = hours + minutes
+    if time < 5:
+        level = "level0"
+    elif time < 10:
+        level = "level1"
+    elif time < 20:
+        level = "level2"
+    elif time < 40:
+        level = "level3"
+    else:
+        level = "level4"
     value = "<span class=\"value\">{}</span>".format(time)
     unit = "<span class=\"unit\">minute{}</span>".format("s" if time > 1 else "")
-    return "{}{}".format(value, unit)
+    return {"closed": False,
+            "level": level,
+            "time_str": "{} {}".format(value, unit)
+            }
